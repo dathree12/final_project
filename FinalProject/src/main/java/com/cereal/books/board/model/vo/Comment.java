f5e994a29ee1ad3d1cdc5bccb6dde54d33ff78d0
kimdm1994 <kdm9982@gmail.com>
2021-04-14 17:21:54 +0900

    board 관련 class 삭제 , comment, header에 js 부트스트랩, owl 추가

--- FinalProject/src/main/java/com/cereal/books/board/model/vo/Comment.java
+++ FinalProject/src/main/java/com/cereal/books/board/model/vo/Comment.java
@@ -1,5 +1,28 @@
 package com.cereal.books.board.model.vo;
 
+import java.sql.Date;
+
 public class Comment {
-	// 댓글
+	
+	private int comNo;
+	
+	private int qaNo;
+	
+	private char comBoard; // char
+	
+	private String comContent;
+	
+	private Date comCreateDate; // java.sql.Date
+	
+	private Date comEditDate;
+	
+	private String comStatus;
+	
+	private String comItem;
+	
+	private int bcNo;
+	
+	private int brNo;
+	
+	private int bfNo;
 }
