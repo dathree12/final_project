7e9ca0fbe19720176ca071e2d07e6e60318eaad8
kimdm1994 <kdm9982@gmail.com>
2021-04-26 11:08:57 +0900

    마지막푸쉬

--- FinalProject/src/main/java/com/cereal/books/board/controller/ClubController.java
+++ FinalProject/src/main/java/com/cereal/books/board/controller/ClubController.java
@@ -52,67 +52,13 @@ public class ClubController {
 		ClubBoard clubBoard = null;
 
 		clubBoard = service.findClubByNo(bcNo);
+		
+		model.addObject("clubBoard", clubBoard);
+		
+		model.setViewName("board/bc_board/bcBoardDetail");
+		
+		return model;
 
-		// 조회수 증가, 쿠키를 이용한 중복 조회수 증가방지
-		Cookie[] cookies = request.getCookies();
-		// 비교하기 위해 새로운 쿠키생성
-		Cookie viewCookie = null;
-
-		// 쿠키가 있을 경우
-		if (cookies != null && cookies.length > 0) {
-			for (int i = 0; i < cookies.length; i++) {
-
-				// Cookie의 name이 cookie + reviewNo와 일치하는 쿠키를 viewCookie에 넣어줌
-				if (cookies[i].getName().equals("cookie" + bcNo)) {
-					System.out.println("처음 쿠키가 생성한 뒤 들어옴.");
-
-					viewCookie = cookies[i];
-				}
-			}
-		}
-
-		if (clubBoard != null) {
-			System.out.println("System - 해당 상세 리뷰페이지로 넘어감");
-
-			model.addObject("clubBoard", clubBoard);
-
-			// 만일 viewCookie가 null일 경우 쿠키를 생성해서 조회수 증가 로직을 처리함.
-			if (viewCookie == null) {
-				System.out.println("cookie 없음");
-
-				// 쿠키 생성(이름, 값)
-				Cookie newCookie = new Cookie("cookie" + bcNo, "|" + bcNo + "|");
-
-				// 쿠키 추가
-				response.addCookie(newCookie);
-
-				// 쿠키를 추가 시키고 조회수 증가시킴
-				int result = service.increaseViewcnt(bcNo);
-
-				if (result > 0) {
-					System.out.println("조회수 증가");
-				} else {
-					System.out.println("조회수 증가 에러");
-				}
-			}
-			// viewCookie가 null이 아닐경우 쿠키가 있으므로 조회수 증가 로직을 처리하지 않음.
-			else {
-				System.out.println("cookie 있음");
-
-				// 쿠키 값 받아옴.
-				String value = viewCookie.getValue();
-
-				System.out.println("cookie 값 : " + value);
-
-			}
-
-			model.setViewName("board/bc_board/bcBoardDetail");
-			return model;
-		} else {
-			// 에러 페이지 설정
-			model.setViewName("/");
-			return model;
-		}
 	}
 
 	// CKEDITOR
