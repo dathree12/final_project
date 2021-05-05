752ad197430bbc8e65d1e23adc61995b1c4bf048
김승현 <kimsh0324@hanmail.net>
2021-05-05 21:38:06 +0900

    210505_승현_책스초코DB all in one 파일, 북펀딩 TestData 추가

---
+++ 북펀딩TestData(4_26).sql
@@ -0,0 +1,411 @@
+-- 북펀딩 데이터 f5 누르면 한번에 적용됩니다.
+
+-- <북펀딩 TestData>
+-- 15개 펀딩중 TestData, 3개 마감된 TestData
+--
+-- <test검색페이지>
+-- kh1~15 까지 -> funding누르면 15부터 내림차순으로 보인다.
+--
+-- 정렬방법은
+-- 가격은 1000원에서 증가
+-- 조회수는 1000에서 감소 -> 인기프로젝트 누르면 1부터 보인다
+-- 인수쌤 감사합니다~~!! -> kh1 이란 제목에 있다. 주의!
+-- 마감날짜는 최근 종료된 것부터 나오도록
+
+
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, 'test검색페이지(kh1)', 1000, '21/04/15', '21/05/15', DEFAULT, NULL, 
+10000, 1000, NULL, 1000, 2, 'P', 'Y', DEFAULT,
+'
+<img alt="" src="/FinalProject/upload/20210422_205139329.gif" style="height:600px; width:600px" />
+<h2><em><strong>인수쌤~ 7개월의 교육 기간, 정말 고생하셨습니다^^<br />
+<br />
+앞으로 더 열심히 노력해서 좋은 개발자가 되도록 하겠습니다.<br />
+<br />
+우리 B 오후반 여러분들도 모두 모두 고생하셨고 취업까지 화이팅입니닷!!<br />
+<br />
+감사합니다~~~~~</strong></em></h2>
+<br><br><br><br><br>', 'testImg.JPG', '20210422_200903406.JPG', NULL
+);
+
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, 'test검색페이지(kh2)', 1000, '21/04/13', '21/05/25', DEFAULT, NULL, 
+10000, 2000, NULL, 950, 2, 'P', 'Y', DEFAULT,
+'
+<img alt="" src="/FinalProject/upload/20210422_201459764.JPG" style="height:400px; width:600px" />
+<br><br><br>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br>
+<br><br><br>
+<img alt="" src="/FinalProject/upload/20210422_201531379.JPG" style="height:227px; width:403px" />
+<br><br><br><br>', 'testImg.JPG', '20210422_200903406.JPG', NULL
+);
+
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, 'test검색페이지(kh3)', 1000, '21/04/05', '21/06/13', DEFAULT, NULL, 
+10000, 3000, NULL, 900, 2, 'P', 'Y', DEFAULT,
+'
+<img alt="" src="/FinalProject/upload/20210422_201459764.JPG" style="height:400px; width:600px" />
+<br><br><br>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br>
+<br><br><br>
+<img alt="" src="/FinalProject/upload/20210422_201531379.JPG" style="height:227px; width:403px" />
+<br><br><br><br>', 'testImg.JPG', '20210422_200903406.JPG', NULL
+);
+
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, 'test검색페이지(kh4)', 1000, '21/03/15', '21/07/15', DEFAULT, NULL, 
+10000, 4000, NULL, 850, 2, 'P', 'Y', DEFAULT,
+'
+<img alt="" src="/FinalProject/upload/20210422_201459764.JPG" style="height:400px; width:600px" />
+<br><br><br>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br>
+<br><br><br>
+<img alt="" src="/FinalProject/upload/20210422_201531379.JPG" style="height:227px; width:403px" />
+<br><br><br><br>', 'testImg.JPG', '20210422_200903406.JPG', NULL
+);
+
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, 'test검색페이지(kh5)', 1000, '21/04/15', '21/07/25', DEFAULT, NULL, 
+10000, 5000, NULL, 800, 2, 'P', 'Y', DEFAULT,
+'
+<img alt="" src="/FinalProject/upload/20210422_201459764.JPG" style="height:400px; width:600px" />
+<br><br><br>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br>
+<br><br><br>
+<img alt="" src="/FinalProject/upload/20210422_201531379.JPG" style="height:227px; width:403px" />
+<br><br><br><br>', 'testImg.JPG', '20210422_200903406.JPG', NULL
+);
+
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, 'test검색페이지(kh6)', 1000, '21/04/15', '21/08/15', DEFAULT, NULL, 
+10000, 6000, NULL, 750, 2, 'P', 'Y', DEFAULT,
+'
+<img alt="" src="/FinalProject/upload/20210422_201459764.JPG" style="height:400px; width:600px" />
+<br><br><br>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br>
+<br><br><br>
+<img alt="" src="/FinalProject/upload/20210422_201531379.JPG" style="height:227px; width:403px" />
+<br><br><br><br>', 'testImg.JPG', '20210422_200903406.JPG', NULL
+);
+
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, 'test검색페이지(kh7)', 1000, '21/02/15', '21/09/05', DEFAULT, NULL, 
+10000, 7000, NULL, 700, 2, 'P', 'Y', DEFAULT,
+'
+<img alt="" src="/FinalProject/upload/20210422_201459764.JPG" style="height:400px; width:600px" />
+<br><br><br>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br>
+<br><br><br>
+<img alt="" src="/FinalProject/upload/20210422_201531379.JPG" style="height:227px; width:403px" />
+<br><br><br><br>', 'testImg.JPG', '20210422_200903406.JPG', NULL
+);
+
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, 'test검색페이지(kh8)', 1000, '21/04/10', '21/10/01', DEFAULT, NULL, 
+10000, 8000, NULL, 600, 2, 'P', 'Y', DEFAULT,
+'
+<img alt="" src="/FinalProject/upload/20210422_201459764.JPG" style="height:400px; width:600px" />
+<br><br><br>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br>
+<br><br><br>
+<img alt="" src="/FinalProject/upload/20210422_201531379.JPG" style="height:227px; width:403px" />
+<br><br><br><br>', 'testImg.JPG', '20210422_200903406.JPG', NULL
+);
+
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, 'test검색페이지(kh9)', 1000, '21/04/15', '21/10/15', DEFAULT, NULL, 
+10000, 9000, NULL, 500, 2, 'P', 'Y', DEFAULT,
+'
+<img alt="" src="/FinalProject/upload/20210422_201459764.JPG" style="height:400px; width:600px" />
+<br><br><br>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br>
+<br><br><br>
+<img alt="" src="/FinalProject/upload/20210422_201531379.JPG" style="height:227px; width:403px" />
+<br><br><br><br>', 'testImg.JPG', '20210422_200903406.JPG', NULL
+);
+
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, 'test검색페이지(kh10)', 1000, '21/04/15', '21/11/05', DEFAULT, NULL, 
+10000, 10000, NULL, 400, 2, 'P', 'Y', DEFAULT,
+'
+<img alt="" src="/FinalProject/upload/20210422_201459764.JPG" style="height:400px; width:600px" />
+<br><br><br>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br>
+<br><br><br>
+<img alt="" src="/FinalProject/upload/20210422_201531379.JPG" style="height:227px; width:403px" />
+<br><br><br><br>', 'testImg.JPG', '20210422_200903406.JPG', NULL
+);
+
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, 'test검색페이지(kh11)', 1000, '21/01/15', '21/11/15', DEFAULT, NULL, 
+10000, 11000, NULL, 300, 2, 'P', 'Y', DEFAULT,
+'
+<img alt="" src="/FinalProject/upload/20210422_201459764.JPG" style="height:400px; width:600px" />
+<br><br><br>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br>
+<br><br><br>
+<img alt="" src="/FinalProject/upload/20210422_201531379.JPG" style="height:227px; width:403px" />
+<br><br><br><br>', 'testImg.JPG', '20210422_200903406.JPG', NULL
+);
+
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, 'test검색페이지(kh12)', 1000, '21/02/15', '21/12/03', DEFAULT, NULL, 
+10000, 12000, NULL, 200, 2, 'P', 'Y', DEFAULT,
+'
+<img alt="" src="/FinalProject/upload/20210422_201459764.JPG" style="height:400px; width:600px" />
+<br><br><br>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br>
+<br><br><br>
+<img alt="" src="/FinalProject/upload/20210422_201531379.JPG" style="height:227px; width:403px" />
+<br><br><br><br>', 'testImg.JPG', '20210422_200903406.JPG', NULL
+);
+
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, 'test검색페이지(kh13)', 1000, '21/04/15', '21/12/15', DEFAULT, NULL, 
+10000, 13000, NULL, 111, 2, 'P', 'Y', DEFAULT,
+'
+<img alt="" src="/FinalProject/upload/20210422_201459764.JPG" style="height:400px; width:600px" />
+<br><br><br>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br>
+<br><br><br>
+<img alt="" src="/FinalProject/upload/20210422_201531379.JPG" style="height:227px; width:403px" />
+<br><br><br><br>', 'testImg.JPG', '20210422_200903406.JPG', NULL
+);
+
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, 'test검색페이지(kh14)', 1000, '21/04/15', '21/12/17', DEFAULT, NULL, 
+10000, 14000, NULL, 50, 2, 'P', 'Y', DEFAULT,
+'
+<img alt="" src="/FinalProject/upload/20210422_201459764.JPG" style="height:400px; width:600px" />
+<br><br><br>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br>
+<br><br><br>
+<img alt="" src="/FinalProject/upload/20210422_201531379.JPG" style="height:227px; width:403px" />
+<br><br><br><br>', 'testImg.JPG', '20210422_200903406.JPG', NULL
+);
+
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, 'test검색페이지(kh15)', 1000, '21/04/15', '21/12/25', DEFAULT, NULL, 
+10000, 15000, NULL, 50, 2, 'P', 'Y', DEFAULT,
+'
+<img alt="" src="/FinalProject/upload/20210422_201459764.JPG" style="height:400px; width:600px" />
+<br><br><br>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br>
+<br><br><br>
+<img alt="" src="/FinalProject/upload/20210422_201531379.JPG" style="height:227px; width:403px" />
+<br><br><br><br>', 'testImg.JPG', '20210422_200903406.JPG', NULL
+);
+
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, 'test마감페이지(1)', 1000, '21/04/15', '21/04/20', DEFAULT, NULL, 
+10000, 15000, NULL, 50, 2, 'P', 'Y', DEFAULT,
+'
+<img alt="" src="/FinalProject/upload/20210422_201459764.JPG" style="height:400px; width:600px" />
+<br><br><br>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br>
+<br><br><br>
+<img alt="" src="/FinalProject/upload/20210422_201531379.JPG" style="height:227px; width:403px" />
+<br><br><br><br>', 'testImg.JPG', '20210422_200903406.JPG', NULL
+);
+
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, 'test마감페이지(2)', 1000, '21/02/15', '21/03/20', DEFAULT, NULL, 
+10000, 17000, NULL, 50, 2, 'P', 'Y', DEFAULT,
+'
+<img alt="" src="/FinalProject/upload/20210422_201459764.JPG" style="height:400px; width:600px" />
+<br><br><br>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br>
+<br><br><br>
+<img alt="" src="/FinalProject/upload/20210422_201531379.JPG" style="height:227px; width:403px" />
+<br><br><br><br>', 'testImg.JPG', '20210422_200903406.JPG', NULL
+);
+
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, 'test마감페이지(3)', 1000, '21/01/01', '21/02/20', DEFAULT, NULL, 
+10000, 19000, NULL, 50, 2, 'P', 'Y', DEFAULT,
+'
+<img alt="" src="/FinalProject/upload/20210422_201459764.JPG" style="height:400px; width:600px" />
+<br><br><br>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br><br>
+<b>test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br></b>
+test페이지입니다/test페이지입니다/test페이지입니다/test페이지입니다/<br>
+<br><br><br>
+<img alt="" src="/FinalProject/upload/20210422_201531379.JPG" style="height:227px; width:403px" />
+<br><br><br><br>', 'testImg.JPG', '20210422_200903406.JPG', NULL
+);
+
+-- 메인페이지 데이터 3개
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, '365일의 하루', 22000, '21/04/01', '21/06/05', DEFAULT, NULL, 
+2200000, 440000, NULL, 50, 20, 'P', 'Y', DEFAULT,
+'
+<p><br />
+<br />
+<var>출판사: 독립출판 소호<br />
+출간일: 2020-12-30<br />
+분야: 예술<br />
+제본: 무선제본<br />
+쪽수: 376p</var></p>
+<br />
+<em><strong>-책 소개-</strong></em><br />
+<br />
+코로나가 시작된 2020년, 7명의 예술가들...
+<p><em style="font-family:Georgia,Times,Times New Roman,serif; font-style:italic">자신의 일상과 기록을 하나의 책으로 엮었습니다.</em></p>
+지나간 어제를 아쉬워하면서...', '365일의 하루1.JPG', '20210425_224314676.JPG', NULL
+);
+
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, '식탁위의 인생', 13000, '21/03/26', '21/05/19', DEFAULT, NULL, 
+1300000, 910000, NULL, 1150, 70, 'P', 'Y', DEFAULT,
+'
+<br />
+<strong>저자: 김수진<br />
+출간일: 2021-03-19<br />
+분야: 에세이<br />
+<br />
+책 소개<br />
+삶을 살아가는 데 있어<br />
+빼놓을 수 없는 것을 묻는다면<br />
+저는 무조건 먹는 것 이라고 말합니다.<br />
+그저 단순히 먹는 것을 좋아하고...</strong>', '식탁위의 인생1.JPG', '20210425_225840429.JPG', NULL
+);
+
+INSERT INTO BOOK_FUNDING_BOARD VALUES(
+SEQ_BF_NO.NEXTVAL, 
+2, '레몬청 만드는 법', 11000, '21/04/15', '21/04/30', DEFAULT, NULL, 
+1100000, 990000, NULL, 2150, 99, 'P', 'Y', DEFAULT,
+'
+<br />
+저자: 김록인 글, 노경무 그림<br />
+출판사: 바다는기다란섬<br />
+출간일: 2020-06-30<br />
+<br />
+책 소개<br />
+괴로우면서도 자꾸 찾게 되는,<br />
+마치 인생처럼 오묘한 맛의 레몬과 라임<br />
+일러스트가 있는 짧은 소설<br />
+〈레몬청 만드는 법〉과 〈핑거라임〉을<br />
+한 권의 책으로 엮었다.', '레몬청만드는법1.JPG', '20210425_230900498.JPG', NULL
+);
+
+COMMIT;
